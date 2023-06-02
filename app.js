8c83cd0a3975d55773d93f8a024ccf5a3a7577c2
Peter-Haung888 <ming31437@gmail.com>
2023-06-02 23:23:52 +0800

    Set a route for rendering index.handlebars and start to listen on server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Include Express and restaurants data
 const express = require('express')
 const app = express()
+const port = 3000
 const restaurants = require('./restaurant.json')
 
 // Include Template engine Handlebars
@@ -11,3 +12,13 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 
 // Change Template engine to Handlebars
 app.set('view engine', 'handlebars')
+
+// Setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// Start and listen on server
+app.listen(port, () => {
+  console.log(`Welcome to my restaurant website: http://localhot:${port}`)
+})
