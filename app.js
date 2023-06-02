6feb990b1f80d7add437d74524a31cc8d1f3da21
Peter-Haung888 <ming31437@gmail.com>
2023-06-02 23:14:30 +0800

    Include Handlebars and set up it

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 const express = require('express')
 const app = express()
 const restaurants = require('./restaurant.json')
+
+// Include Template engine Handlebars
+const exphbs = require('express-handlebars')
+
+// Set Template engine Layout
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+
+// Change Template engine to Handlebars
+app.set('view engine', 'handlebars')
