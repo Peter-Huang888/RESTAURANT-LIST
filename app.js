1c108c8f580b47eb5b2e21ea406fff28c27f6526
Peter-Haung888 <ming31437@gmail.com>
2023-06-13 19:05:53 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 const restaurants = require('./restaurant.json').results
+const mongoose = require('mongoose') //載入mongoose
+// 如果在非正式環境下，載入dotenv
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) //設定連線到 mongoDB
+
+const db = mongoose.connection //取得資料庫連線狀態
+// 連線失敗
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('mongoDB connected')
+})
+
 
 // Include Template engine Handlebars
 const exphbs = require('express-handlebars')
