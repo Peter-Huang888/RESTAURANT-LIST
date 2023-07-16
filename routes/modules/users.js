3436c6f36388aac5a9ff54b1ec2981c2d516842d
Peter-Haung888 <ming31437@gmail.com>
2023-07-16 20:01:38 +0800

    feat: add login and register pages

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,8 +5,14 @@ router.get('/login', (req, res) => {
   res.render('Login')
 })
 
+router.post('/login', (req,res) => {
+
+})
 router.get('/register', (req, res) => {
   res.render('Register')
+})
+router.post('/register', (req,res) => {
+  
 })
 
 module.exports = router
